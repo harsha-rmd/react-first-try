abf93d32b51e5a0c3a9d5c02b09edbb741cd2e40
Harsha <harsha_rmd@live.com>
2018-06-16 20:04:00 +0530

    modal resize completed.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Modal, Button } from 'react-bootstrap';
+import Draggable from 'react-draggable';
 import './App.css';
 
 class App extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      show: true,
+      popupBodyHeight: 200
+    };
+    this.mouseDown = false;
+    this.initialMouseDown = null;
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+    this.handleMouseDown = this.handleMouseDown.bind(this);
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+    this.handleMouseUp = this.handleMouseUp.bind(this);
+  }
+
+  handleMouseDown(event) {
+    this.mouseDown = true;
+    this.initialMouseDown = event.clientY;
+    this.popupBodyHeight = this.state.popupBodyHeight;
+  }
+  handleMouseUp(){
+    this.mouseDown = false;
+  }
+  handleMouseMove(event){
+    if(this.mouseDown){
+      let computedHeight = this.popupBodyHeight+event.clientY-this.initialMouseDown;
+      if(computedHeight <= 120){
+        computedHeight = 121;
+      } else if(computedHeight >= 300) {
+        computedHeight = 299;
+      }
+      this.setState({popupBodyHeight: computedHeight});
+    }
+  }
+
+  handleClose() {
+    this.setState({ show: false });
+  }
+
+  handleShow() {
+    this.setState({ show: true });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <section>
+        <Modal show={this.state.show} onHide={this.handleClose} onMouseUp={()=>{this.handleMouseUp()}} onMouseMove={(event)=>{this.handleMouseMove(event)}}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body style={{height:this.state.popupBodyHeight+'px'}}>
+            <h4>Text in a modal</h4>
+            <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.handleClose}>Close</Button>
+            <div
+              className='handle'
+              onMouseDown={(event)=>{this.handleMouseDown(event)}}
+            >
+            </div>
+          </Modal.Footer>
+        </Modal>
+      </section>
     );
   }
 }
